200d5d6a6660fb09121d623b1738d5cf411e7d8d
raprapchh <raph.chanliongco@outlook.com>
2025-11-17 17:32:19 +0100

    Trigger CI check creation

---
+++ build/CMakeFiles/r-type_server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/r-type_server.dir/link.d"
+  "CMakeFiles/r-type_server.dir/src/server/main.cpp.o"
+  "CMakeFiles/r-type_server.dir/src/server/main.cpp.o.d"
+  "r-type_server"
+  "r-type_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/r-type_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
