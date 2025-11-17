32aca5c7b998698bfbf6c17afd4d610422be5b9c
raprapchh <raph.chanliongco@outlook.com>
2025-11-17 17:32:19 +0100

    chore(ci): trigger CI check creation

---
+++ build/CMakeFiles/r-type_client.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/r-type_client.dir/link.d"
+  "CMakeFiles/r-type_client.dir/src/client/main.cpp.o"
+  "CMakeFiles/r-type_client.dir/src/client/main.cpp.o.d"
+  "r-type_client"
+  "r-type_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/r-type_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
