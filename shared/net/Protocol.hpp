8c64be1cde2830add21eaf748f829c24fc734a7e
YlanCuvier <ylan.cuvier@epitech.eu>
2025-11-20 14:59:08 +0100

    fix(server): corrected the sharde files so they respect the clang format too

--- shared/net/Protocol.hpp
+++ shared/net/Protocol.hpp
@@ -4,17 +4,18 @@
 
 namespace rtype::net {
 
-    enum class MessageType : uint16_t {
-        PlayerJoin = 1,
-        PlayerMove = 2,
-        PlayerShoot = 3,
-        PlayerLeave = 4,
-        EntitySpawn = 5,
-        EntityMove = 6,
-        EntityDestroy = 7,
-        GameStart = 8,
-        GameState = 9,
-        Ping = 10,
-        Pong = 11
-    };
-}
+enum class MessageType : uint16_t {
+    PlayerJoin = 1,
+    PlayerMove = 2,
+    PlayerShoot = 3,
+    PlayerLeave = 4,
+    EntitySpawn = 5,
+    EntityMove = 6,
+    EntityDestroy = 7,
+    GameStart = 8,
+    GameState = 9,
+    Ping = 10,
+    Pong = 11
+};
+
+} // namespace rtype::net
