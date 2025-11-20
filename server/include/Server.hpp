f3cb13401e340bb4cb0024563e65bcb9f93375a1
YlanCuvier <ylan.cuvier@epitech.eu>
2025-11-20 14:58:41 +0100

    fix(server): corrected the server files so they respect the clang format

--- server/include/Server.hpp
+++ server/include/Server.hpp
@@ -1,44 +1,45 @@
 #pragma once
 
-#include "UdpServer.hpp"
 #include "../shared/net/Packet.hpp"
 #include "../shared/net/Protocol.hpp"
-#include <memory>
+#include "UdpServer.hpp"
 #include <map>
+#include <memory>
 #include <string>
 
 namespace rtype::server {
 
-    struct ClientInfo {
-        std::string ip;
-        uint16_t port;
-        uint32_t player_id;
-        bool is_connected;
-    };
+struct ClientInfo {
+    std::string ip;
+    uint16_t port;
+    uint32_t player_id;
+    bool is_connected;
+};
 
-    class Server {
-    public:
-        Server(uint16_t port = 4242);
-        ~Server();
+class Server {
+public:
+    Server(uint16_t port = 4242);
+    ~Server();
 
-        void start();
-        void stop();
-        void run();
+    void start();
+    void stop();
+    void run();
 
-    private:
-        void handle_client_message(const std::string& client_ip, uint16_t client_port,
-                                  const std::vector<uint8_t>& data);
-        void handle_player_join(const std::string& client_ip, uint16_t client_port);
-        void handle_player_move(const std::string& client_ip, uint16_t client_port,
+private:
+    void handle_client_message(const std::string& client_ip, uint16_t client_port,
                                const std::vector<uint8_t>& data);
-        void broadcast_message(const std::vector<uint8_t>& data,
-                             const std::string& exclude_ip = "", uint16_t exclude_port = 0);
+    void handle_player_join(const std::string& client_ip, uint16_t client_port);
+    void handle_player_move(const std::string& client_ip, uint16_t client_port,
+                            const std::vector<uint8_t>& data);
+    void broadcast_message(const std::vector<uint8_t>& data, const std::string& exclude_ip = "",
+                           uint16_t exclude_port = 0);
 
-        uint16_t port_;
-        std::unique_ptr<asio::io_context> io_context_;
-        std::unique_ptr<UdpServer> udp_server_;
-        std::map<std::string, ClientInfo> clients_;
-        uint32_t next_player_id_;
-        bool running_;
-    };
-}
+    uint16_t port_;
+    std::unique_ptr<asio::io_context> io_context_;
+    std::unique_ptr<UdpServer> udp_server_;
+    std::map<std::string, ClientInfo> clients_;
+    uint32_t next_player_id_;
+    bool running_;
+};
+
+} // namespace rtype::server
--- server/src/Server.cpp
+++ server/src/Server.cpp
@@ -5,127 +5,120 @@
 
 namespace rtype::server {
 
-    Server::Server(uint16_t port)
-        : port_(port), next_player_id_(1), running_(false) {
-        io_context_ = std::make_unique<asio::io_context>();
-        udp_server_ = std::make_unique<UdpServer>(*io_context_, port_);
+Server::Server(uint16_t port)
+    : port_(port), next_player_id_(1), running_(false) {
+    io_context_ = std::make_unique<asio::io_context>();
+    udp_server_ = std::make_unique<UdpServer>(*io_context_, port_);
+}
+
+Server::~Server() { stop(); }
+
+void Server::start() {
+    if (running_) {
+        return;
     }
 
-    Server::~Server() {
-        stop();
+    running_ = true;
+    udp_server_->set_message_handler(
+        [this](const std::string& ip, uint16_t port, const std::vector<uint8_t>& data) {
+            handle_client_message(ip, port, data);
+        });
+
+    udp_server_->start();
+    std::cout << "Server started on port " << port_ << std::endl;
+}
+
+void Server::stop() {
+    running_ = false;
+    if (udp_server_) {
+        udp_server_->stop();
     }
+}
 
-    void Server::start() {
-        if (running_) {
-            return;
-        }
-
-        running_ = true;
-        udp_server_->set_message_handler(
-            [this](const std::string& ip, uint16_t port, const std::vector<uint8_t>& data) {
-                handle_client_message(ip, port, data);
-            }
-        );
-
-        udp_server_->start();
-        std::cout << "Server started on port " << port_ << std::endl;
+void Server::run() {
+    start();
+    if (io_context_) {
+        io_context_->run();
     }
+}
 
-    void Server::stop() {
-        running_ = false;
-        if (udp_server_) {
-            udp_server_->stop();
-        }
-    }
-
-    void Server::run() {
-        start();
-        if (io_context_) {
-            io_context_->run();
-        }
-    }
-
-    void Server::handle_client_message(const std::string& client_ip, uint16_t client_port,
-                                      const std::vector<uint8_t>& data) {
-        rtype::net::Packet packet = rtype::net::Packet::deserialize(data);
-
-        std::string client_key = client_ip + ":" + std::to_string(client_port);
-
-        switch (static_cast<rtype::net::MessageType>(packet.header.message_type)) {
-            case rtype::net::MessageType::PlayerJoin:
-                handle_player_join(client_ip, client_port);
-                break;
-
-            case rtype::net::MessageType::PlayerMove:
-                handle_player_move(client_ip, client_port, packet.body);
-                break;
-
-            case rtype::net::MessageType::Ping:
-                {
-                    rtype::net::Serializer serializer;
-                    serializer.write(static_cast<uint16_t>(rtype::net::MessageType::Pong));
-                    rtype::net::Packet pong_packet(
-                        static_cast<uint16_t>(rtype::net::MessageType::Pong),
-                        serializer.get_data()
-                    );
-                    udp_server_->send(client_ip, client_port, pong_packet.serialize());
-                }
-                break;
-
-            default:
-                std::cout << "Unknown message type: " << packet.header.message_type << std::endl;
-        }
-    }
-
-    void Server::handle_player_join(const std::string& client_ip, uint16_t client_port) {
-        std::string client_key = client_ip + ":" + std::to_string(client_port);
-
-        if (clients_.find(client_key) != clients_.end()) {
-            return;
-        }
-
-        ClientInfo info;
-        info.ip = client_ip;
-        info.port = client_port;
-        info.player_id = next_player_id_++;
-        info.is_connected = true;
-
-        clients_[client_key] = info;
-
-        std::cout << "Player " << info.player_id << " joined from " << client_ip << ":"
-                  << client_port << std::endl;
-
-        rtype::net::Serializer serializer;
-        serializer.write(info.player_id);
-
-        rtype::net::Packet response(
-            static_cast<uint16_t>(rtype::net::MessageType::PlayerJoin),
-            serializer.get_data()
-        );
-
-        udp_server_->send(client_ip, client_port, response.serialize());
-        broadcast_message(response.serialize(), client_ip, client_port);
-    }
-
-    void Server::handle_player_move(const std::string& client_ip, uint16_t client_port,
+void Server::handle_client_message(const std::string& client_ip, uint16_t client_port,
                                    const std::vector<uint8_t>& data) {
-        std::string client_key = client_ip + ":" + std::to_string(client_port);
+    rtype::net::Packet packet = rtype::net::Packet::deserialize(data);
 
-        auto it = clients_.find(client_key);
-        if (it == clients_.end()) {
-            return;
-        }
+    std::string client_key = client_ip + ":" + std::to_string(client_port);
 
-        broadcast_message(data, client_ip, client_port);
+    switch (static_cast<rtype::net::MessageType>(packet.header.message_type)) {
+        case rtype::net::MessageType::PlayerJoin:
+            handle_player_join(client_ip, client_port);
+            break;
+
+        case rtype::net::MessageType::PlayerMove:
+            handle_player_move(client_ip, client_port, packet.body);
+            break;
+
+        case rtype::net::MessageType::Ping: {
+            rtype::net::Serializer serializer;
+            serializer.write(static_cast<uint16_t>(rtype::net::MessageType::Pong));
+            rtype::net::Packet pong_packet(
+                static_cast<uint16_t>(rtype::net::MessageType::Pong),
+                serializer.get_data());
+            udp_server_->send(client_ip, client_port, pong_packet.serialize());
+        } break;
+
+        default:
+            std::cout << "Unknown message type: " << packet.header.message_type << std::endl;
+    }
+}
+
+void Server::handle_player_join(const std::string& client_ip, uint16_t client_port) {
+    std::string client_key = client_ip + ":" + std::to_string(client_port);
+
+    if (clients_.find(client_key) != clients_.end()) {
+        return;
     }
 
-    void Server::broadcast_message(const std::vector<uint8_t>& data,
-                                  const std::string& exclude_ip, uint16_t exclude_port) {
-        for (const auto& [key, client] : clients_) {
-            if (client.is_connected &&
-                (client.ip != exclude_ip || client.port != exclude_port)) {
-                udp_server_->send(client.ip, client.port, data);
-            }
+    ClientInfo info;
+    info.ip = client_ip;
+    info.port = client_port;
+    info.player_id = next_player_id_++;
+    info.is_connected = true;
+
+    clients_[client_key] = info;
+
+    std::cout << "Player " << info.player_id << " joined from " << client_ip << ":"
+              << client_port << std::endl;
+
+    rtype::net::Serializer serializer;
+    serializer.write(info.player_id);
+
+    rtype::net::Packet response(static_cast<uint16_t>(rtype::net::MessageType::PlayerJoin),
+                                serializer.get_data());
+
+    udp_server_->send(client_ip, client_port, response.serialize());
+    broadcast_message(response.serialize(), client_ip, client_port);
+}
+
+void Server::handle_player_move(const std::string& client_ip, uint16_t client_port,
+                                const std::vector<uint8_t>& data) {
+    std::string client_key = client_ip + ":" + std::to_string(client_port);
+
+    auto it = clients_.find(client_key);
+    if (it == clients_.end()) {
+        return;
+    }
+
+    broadcast_message(data, client_ip, client_port);
+}
+
+void Server::broadcast_message(const std::vector<uint8_t>& data, const std::string& exclude_ip,
+                               uint16_t exclude_port) {
+    for (const auto& [key, client] : clients_) {
+        if (client.is_connected &&
+            (client.ip != exclude_ip || client.port != exclude_port)) {
+            udp_server_->send(client.ip, client.port, data);
         }
     }
 }
+
+} // namespace rtype::server
