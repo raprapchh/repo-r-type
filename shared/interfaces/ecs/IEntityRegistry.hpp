d2d15c0d5d4220ea95e470f7cdcf7a060b1408ee
raprapchh <raph.chanliongco@outlook.com>
2025-11-24 18:41:55 +0100

    fix: clang-format in the files registry.hpp and .cpp

--- shared/interfaces/ecs/IEntityRegistry.hpp
+++ shared/interfaces/ecs/IEntityRegistry.hpp
@@ -7,27 +7,27 @@ namespace GameEngine {
 using entity_t = std::size_t;
 
 class IEntityRegistry {
-    public:
-        virtual ~IEntityRegistry() = default;
+  public:
+    virtual ~IEntityRegistry() = default;
 
-        /**
-         * @brief Creates a new entity.
-         * @return The ID of the created entity.
-        */
-        virtual entity_t createEntity() = 0;
+    /**
+     * @brief Creates a new entity.
+     * @return The ID of the created entity.
+     */
+    virtual entity_t createEntity() = 0;
 
-        /**
-         * @brief Destroys an entity.
-         * @param entity The ID of the entity to destroy.
-        */
-        virtual void destroyEntity(entity_t entity) = 0;
+    /**
+     * @brief Destroys an entity.
+     * @param entity The ID of the entity to destroy.
+     */
+    virtual void destroyEntity(entity_t entity) = 0;
 
-        /**
-         * @brief Checks if an entity is valid.
-         * @param entity The ID of the entity to check.
-         * @return True if the entity is valid, false otherwise.
-        */
-        virtual bool isValid(entity_t entity) const = 0;
+    /**
+     * @brief Checks if an entity is valid.
+     * @param entity The ID of the entity to check.
+     * @return True if the entity is valid, false otherwise.
+     */
+    virtual bool isValid(entity_t entity) const = 0;
 };
 
 } // namespace GameEngine
