3fa922b4b9448ec80d6d4b4c763fd4917f3bf36e
raprapchh <raph.chanliongco@outlook.com>
2025-12-02 23:42:36 +0100

    feat(client): introduce new sprites, core ECS components and systems, and refactor server to integrate with ECS registry and new client  and also the old commit that make the ci/cd crashed

---
+++ client/include/UdpClient.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <asio.hpp>
+#include <atomic>
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace rtype::client {
+
+class UdpClient {
+public:
+    using message_callback = std::function<void(const asio::error_code&, std::size_t, const std::vector<uint8_t>&)>;
+
+    UdpClient(asio::io_context& io_context, const std::string& host, uint16_t port);
+    ~UdpClient();
+
+    void start_receive();
+    void send(const std::vector<uint8_t>& data);
+    void stop();
+
+    void set_message_handler(message_callback handler);
+
+private:
+    void handle_receive(const asio::error_code& error, std::size_t bytes_transferred);
+
+    asio::io_context& io_context_;
+    std::unique_ptr<asio::ip::udp::socket> socket_;
+    asio::ip::udp::endpoint server_endpoint_;
+    std::vector<uint8_t> recv_buffer_;
+    message_callback handler_;
+    std::atomic<bool> running_;
+};
+
+} // namespace rtype::client
--- server/include/Server.hpp
+++ server/include/Server.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "UdpServer.hpp"
-#include "Instance.hpp"
+#include "../../ecs/include/Registry.hpp"
 #include "../../shared/interfaces/network/IProtocolAdapter.hpp"
 #include "../../shared/interfaces/network/IMessageSerializer.hpp"
 #include "../../shared/net/Packet.hpp"
@@ -21,11 +21,12 @@ struct ClientInfo {
     uint16_t port;
     uint32_t player_id;
     bool is_connected;
+    GameEngine::entity_t entity_id;
 };
 
 class Server {
   public:
-    Server(Instance& instance, uint16_t port = 4242);
+    Server(GameEngine::Registry& registry, uint16_t port = 4242);
     ~Server();
 
     void start();
@@ -58,7 +59,7 @@ class Server {
     static constexpr std::chrono::milliseconds TICK_DURATION =
         std::chrono::milliseconds(static_cast<long>(1000.0 / TARGET_TICK_RATE));
 
-    Instance& instance_;
+    GameEngine::Registry& registry_;
 };
 
 } // namespace rtype::server
