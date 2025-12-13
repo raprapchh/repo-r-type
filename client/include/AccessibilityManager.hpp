a5ebcfe50c773b46d1e4cd1fbd3b5d293828fc51
raprapchh <raph.chanliongco@outlook.com>
2025-12-13 18:35:08 +0100

    feat(accessibility): implement multi-colorblind mode support with 4 modes (None, Deuteranopia, Protanopia, Tritanopia) including cycle navigation and dedicated color palettes for players and enemies

--- client/include/AccessibilityManager.hpp
+++ client/include/AccessibilityManager.hpp
@@ -1,24 +1,25 @@
 #pragma once
 
-#include "../../shared/net/MessageData.hpp"
-#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics.hpp>
 #include <cstdint>
+#include <string>
 
 namespace rtype::client {
+
+enum class ColorBlindMode { None, Deuteranopia, Protanopia, Tritanopia };
 
 class AccessibilityManager {
   public:
     AccessibilityManager();
     ~AccessibilityManager() = default;
 
-    void toggle_color_blind_mode();
-
-    bool is_color_blind_mode_active() const;
-
+    void cycle_mode();
+    ColorBlindMode get_current_mode() const;
+    std::string get_mode_name() const;
     sf::Color get_entity_color(uint16_t type) const;
 
   private:
-    bool is_color_blind_mode_;
+    ColorBlindMode current_mode_;
 };
 
 } // namespace rtype::client
