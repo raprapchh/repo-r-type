34ac1bd5f45721c7eb55544ef3e62287512cc3c6
YlanCuvier <ylan.cuvier@epitech.eu>
2026-01-07 00:55:12 +0100

    fix(compilation): correct compilation error

--- server/include/ClientInfo.hpp
+++ server/include/ClientInfo.hpp
@@ -10,6 +10,7 @@ struct ClientInfo {
     std::string ip;
     uint16_t port;
     uint32_t player_id;
+    std::string player_name;
     bool is_connected;
     GameEngine::entity_t entity_id;
     std::chrono::steady_clock::time_point last_seen;
