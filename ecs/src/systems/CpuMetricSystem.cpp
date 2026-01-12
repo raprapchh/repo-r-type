fe9180d6050e66c3c8216f2b5d4224b5b409957e
raprapchh <raph.chanliongco@outlook.com>
2026-01-13 00:53:17 +0100

    feat(ecs): implement CpuMetricSystem for frame time monitoring

---
+++ ecs/src/systems/CpuMetricSystem.cpp
@@ -0,0 +1,26 @@
+#include "../../include/systems/CpuMetricSystem.hpp"
+#include "../../include/components/CpuStats.hpp"
+#include "../../include/components/TextDrawable.hpp"
+#include "../../include/components/UITag.hpp"
+#include <iomanip>
+#include <sstream>
+
+namespace rtype::ecs {
+
+void CpuMetricSystem::update(GameEngine::Registry& registry, double dt) {
+    (void)dt;
+    auto view = registry.view<component::CpuStats, component::TextDrawable, component::UITag>();
+
+    for (auto entity : view) {
+        auto& cpuStats = registry.getComponent<component::CpuStats>(entity);
+        auto& textDrawable = registry.getComponent<component::TextDrawable>(entity);
+
+        cpuStats.frameTimeMs = static_cast<float>(dt * 1000.0);
+
+        std::stringstream ss;
+        ss << "CPU: " << std::fixed << std::setprecision(2) << cpuStats.frameTimeMs << " ms";
+        textDrawable.text.setString(ss.str());
+    }
+}
+
+} // namespace rtype::ecs
